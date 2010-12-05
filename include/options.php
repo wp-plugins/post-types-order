3bbe9ea17bef40e25d27c1b93d45ad08ef7272c8
nsp-code <nsp-code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-05 00:29:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post-types-order/trunk@319126 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/options.php
+++ include/options.php
@@ -9,8 +9,11 @@ function cpt_plugin_options()
             {
                     
                 $options['level'] = $_POST['level'];
+                
+                $options['autosort']    = isset($_POST['autosort'])     ? $_POST['autosort']    : '';
+                $options['adminsort']   = isset($_POST['adminsort'])    ? $_POST['adminsort']   : '';
                     
-                echo '<div class="updated fade"><p>Settings Saved</p></div>';
+                echo '<div class="updated fade"><p>' . __('Settings Saved', 'cpt') . '</p></div>';
 
                 update_option('cpto_options', $options);   
             }
@@ -32,21 +35,76 @@ function cpt_plugin_options()
                                             <th scope="row" style="text-align: right;"><label>Minimum Level to use this plugin</label></th>
                                             <td>
                                                 <select id="role" name="level">
-                                                    <option value="0" <?php if ($options['level'] == "0") echo 'selected="selected"'?>>Subscriber</option>
-                                                    <option value="1" <?php if ($options['level'] == "1") echo 'selected="selected"'?>>Contributor</option>
-                                                    <option value="2" <?php if ($options['level'] == "2") echo 'selected="selected"'?>>Author</option>
-                                                    <option value="5" <?php if ($options['level'] == "5") echo 'selected="selected"'?>>Editor</option>
-                                                    <option value="8" <?php if ($options['level'] == "8") echo 'selected="selected"'?>>Administrator</option>
+                                                    <option value="0" <?php if ($options['level'] == "0") echo 'selected="selected"'?>><?php _e('Subscriber', 'cpt') ?></option>
+                                                    <option value="1" <?php if ($options['level'] == "1") echo 'selected="selected"'?>><?php _e('Contributor', 'cpt') ?></option>
+                                                    <option value="2" <?php if ($options['level'] == "2") echo 'selected="selected"'?>><?php _e('Author', 'cpt') ?></option>
+                                                    <option value="5" <?php if ($options['level'] == "5") echo 'selected="selected"'?>><?php _e('Editor', 'cpt') ?></option>
+                                                    <option value="8" <?php if ($options['level'] == "8") echo 'selected="selected"'?>><?php _e('Administrator', 'cpt') ?></option>
                                                 </select>
                                             </td>
                                         </tr>
-                                                                                
+                                        
+                                        <tr valign="top">
+                                            <th scope="row" style="text-align: right;"><label>Auto Sort</label></th>
+                                            <td>
+                                                <label for="users_can_register">
+                                                <input type="checkbox" <?php if ($options['autosort'] == "1") {echo ' checked="checked"';} ?> value="1" name="autosort">
+                                                <?php _e("If checked, the plug-in will automatically update the wp-queries to use the new order (<b>No code update is necessarily</b>).<br /> If you need more order customizations you will need to uncheck this and include 'menu_order' into your theme queries", 'cpt') ?>.</label>
+                                                
+                                                <p><a href="javascript:;" onclick="jQuery('#example1').slideToggle();;return false;">Show Examples</a></p>
+                                                <div id="example1" style="display: none">
+                                                
+                                                <p class="example"><br /><?php _e('The following PHP code will still return the post in the set-up Order', 'cpt') ?>:</p>
+                                                <pre class="example">
+$args = array(
+              'post_type' => 'feature'
+            );
+
+$my_query = new WP_Query($args);
+while ($my_query->have_posts())
+    {
+        $my_query->the_post();
+        (..your code..)          
+    }
+</pre>
+<p class="example"><br /><?php _e('Or', 'cpt') ?>:</p>
+                                                <pre class="example">
+$posts = get_posts($args);
+foreach ($posts as $post)
+    {
+        (..your code..)     
+    }
+                                                </pre>
+                                                
+<p class="example"><br /><?php _e('If the Auto Sort is uncheck you will need to use the "orderby" and "order" parameters', 'cpt') ?>:</p>
+<pre class="example">
+$args = array(
+              'post_type' => 'feature',
+              'orderby'   => 'menu_order',
+              'order'     => 'ASC'
+            );
+</pre>
+                                                
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        
+                                        
+                                        <tr valign="top">
+                                            <th scope="row" style="text-align: right;"><label>Admin Sort</label></th>
+                                            <td>
+                                                <label for="users_can_register">
+                                                <input type="checkbox" <?php if ($options['adminsort'] == "1") {echo ' checked="checked"';} ?> value="1" name="adminsort">
+                                                <?php _e("To affect the admin interface, to see the post types per your new sort, this need to be checked", 'cpt') ?>.</label>
+                                            </td>
+                                        </tr>
+                                        
                                     </tbody>
                                 </table>
                                
                     
                                 <p class="submit">
-                                    <input type="submit" name="Submit" class="button-primary" value="Save Settings">
+                                    <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Settings', 'cpt') ?>">
                                </p>
                             
                                 <input type="hidden" name="form_submit" value="true" />
--- post-types-order.php
+++ post-types-order.php
@@ -5,33 +5,103 @@ Plugin URI: http://www.nsp-code.com
 Description: Order Post Types Objects using a Drag and Drop Sortable javascript capability
 Author: NSP CODE
 Author URI: http://www.nsp-code.com 
-Version: 1.1.2
+Version: 1.3.8
 */
 
 define('CPTPATH', ABSPATH.'wp-content/plugins/post-types-order');
 define('CPTURL', get_option('siteurl').'/wp-content/plugins/post-types-order');
 
 
+register_deactivation_hook(__FILE__, 'CPTO_deactivated');
+register_activation_hook(__FILE__, 'CPTO_activated');
+
+function CPTO_activated() 
+    {
+        //make sure the vars are set as default
+        $options = get_option('cpto_options');
+        if (!isset($options['autosort']))
+            $options['autosort'] = '1';
+            
+        if (!isset($options['adminsort']))
+            $options['adminsort'] = '1';
+            
+        update_option('cpto_options', $options);
+    }
+
+function CPTO_deactivated() 
+    {
+        
+    }
+
+add_filter('pre_get_posts', 'CPTO_pre_get_posts');
+function CPTO_pre_get_posts($query)
+    {
+       
+        $options = get_option('cpto_options');
+        if (is_admin())
+            {
+                //no need if it's admin interface
+                return false;   
+            }
+        //if auto sort    
+        if ($options['autosort'] == "1")
+            {
+                //remove the supresed filters;
+                if (isset($query->query['suppress_filters']))
+                    $query->query['suppress_filters'] = FALSE;    
+                
+                if (isset($query->query_vars['suppress_filters']))
+                    $query->query_vars['suppress_filters'] = FALSE;
+            }
+            
+        return $query;
+    }
+
 add_filter('posts_orderby', 'CPTOrderPosts');
 function CPTOrderPosts($orderBy) 
     {
         global $wpdb;
-        $orderBy = "{$wpdb->posts}.menu_order, {$wpdb->posts}.post_date DESC";
+        
+        $options = get_option('cpto_options');
+        
+        if (is_admin())
+                {
+                    if ($options['adminsort'] == "1")
+                        $orderBy = "{$wpdb->posts}.menu_order, {$wpdb->posts}.post_date DESC";
+                }
+            else
+                {
+                    if ($options['autosort'] == "1")
+                        $orderBy = "{$wpdb->posts}.menu_order, {$wpdb->posts}.post_date DESC";
+                }
+
         return($orderBy);
     }
 
     
-add_action('wp_loaded', 'initCPT' );
-add_action('admin_menu', 'cpt_plugin_menu');
+add_action('wp_loaded', 'initCPTO' );
+add_action('admin_menu', 'cpto_plugin_menu');
+
+add_action( 'plugins_loaded', 'cpto_load_textdomain', 2 );
+
+
+function cpto_load_textdomain() 
+    {
+        $locale = get_locale();
+        $mofile = CPTPATH . '/lang/cpt-' . $locale . '.mo';
+        if ( file_exists( $mofile ) ) {
+            load_textdomain( 'cppt', $mofile );
+        }
+    }
   
 
-function cpt_plugin_menu() 
+function cpto_plugin_menu() 
     {
         include (CPTPATH . '/include/options.php');
         add_options_page('Post Types Order', 'Post Types Order', 'manage_options', 'cpto-options', 'cpt_plugin_options');
     }
 	
-function initCPT() 
+function initCPTO() 
     {
 	    global $custom_post_type_order, $userdata;
 
@@ -40,69 +110,36 @@ function initCPT()
                 {
                     global $userdata;
                     if ($userdata->user_level >= $options['level'])
-                    $custom_post_type_order = new CPT();     
+                    $custom_post_type_order = new CPTO();     
                 }
             else
                 {
                     if (is_admin())
                         {
-                            $custom_post_type_order = new CPT();
+                            $custom_post_type_order = new CPTO();
                         }        
                 }
     }
     
     
-class Custom_Type_Order_Walker extends Walker 
+class Post_Types_Order_Walker extends Walker 
     {
-        /**
-         * @see Walker::$tree_type
-         * @since 2.1.0
-         * @var string
-         */
-        var $tree_type = 'page';
 
-        /**
-         * @see Walker::$db_fields
-         * @since 2.1.0
-         * @todo Decouple this.
-         * @var array
-         */
         var $db_fields = array ('parent' => 'post_parent', 'id' => 'ID');
 
-        /**
-         * @see Walker::start_lvl()
-         * @since 2.1.0
-         *
-         * @param string $output Passed by reference. Used to append additional content.
-         * @param int $depth Depth of page. Used for padding.
-         */
+
         function start_lvl(&$output, $depth) {
             $indent = str_repeat("\t", $depth);
             $output .= "\n$indent<ul class='children'>\n";
         }
 
-        /**
-         * @see Walker::end_lvl()
-         * @since 2.1.0
-         *
-         * @param string $output Passed by reference. Used to append additional content.
-         * @param int $depth Depth of page. Used for padding.
-         */
+
         function end_lvl(&$output, $depth) {
             $indent = str_repeat("\t", $depth);
             $output .= "$indent</ul>\n";
         }
 
-        /**
-         * @see Walker::start_el()
-         * @since 2.1.0
-         *
-         * @param string $output Passed by reference. Used to append additional content.
-         * @param object $page Page data object.
-         * @param int $depth Depth of page. Used for padding.
-         * @param int $current_page Page ID.
-         * @param array $args
-         */
+
         function start_el(&$output, $page, $depth, $args) {
             if ( $depth )
                 $indent = str_repeat("\t", $depth);
@@ -114,14 +151,7 @@ class Custom_Type_Order_Walker extends Walker
             $output .= $indent . '<li id="item_'.$page->ID.'"><span>'.apply_filters( 'the_title', $page->post_title, $page->ID ).'</span>';
         }
 
-        /**
-         * @see Walker::end_el()
-         * @since 2.1.0
-         *
-         * @param string $output Passed by reference. Used to append additional content.
-         * @param object $page Page data object. Not used.
-         * @param int $depth Depth of page. Not Used.
-         */
+
         function end_el(&$output, $page, $depth) {
             $output .= "</li>\n";
         }
@@ -129,11 +159,11 @@ class Custom_Type_Order_Walker extends Walker
     }
 
 
-class CPT 
+class CPTO 
     {
 	    var $current_post_type = null;
 	    
-	    function CPT() 
+	    function CPTO() 
             {
 		        add_action( 'admin_init', array(&$this, 'registerFiles'), 11 );
                 add_action( 'admin_init', array(&$this, 'checkPost'), 10 );
@@ -158,9 +188,9 @@ class CPT
 	    
 	    function checkPost() 
             {
-		        if ( isset($_GET['page']) && substr($_GET['page'], 0, 16) == 'order-post-type-' ) 
+		        if ( isset($_GET['page']) && substr($_GET['page'], 0, 17) == 'order-post-types-' ) 
                     {
-			            $this->current_post_type = get_post_type_object(str_replace( 'order-post-type-', '', $_GET['page'] ));
+			            $this->current_post_type = get_post_type_object(str_replace( 'order-post-types-', '', $_GET['page'] ));
 			            if ( $this->current_post_type == null) 
                             {
 				                wp_die('Invalid post type');
@@ -172,10 +202,10 @@ class CPT
             {
 		        global $wpdb;
 		        
-		        parse_str($_POST['order'], $output);
+		        parse_str($_POST['order'], $data);
 		        
-		        if (is_array($output))
-                foreach($output as $key => $values ) 
+		        if (is_array($data))
+                foreach($data as $key => $values ) 
                     {
 			            if ( $key == 'item' ) 
                             {
@@ -202,75 +232,79 @@ class CPT
                 $post_types = get_post_types();
                 foreach( $post_types as $post_type_name ) 
                     {
+                        if ($post_type_name == 'page')
+                            continue;
+                        
                         if ($post_type_name == 'post')
-                            add_submenu_page('edit.php', 'Re-Order', 'Re-Order', $userdata->user_level, 'order-post-type-'.$post_type_name, array(&$this, 'pageManage') );
+                            add_submenu_page('edit.php', 'Re-Order', 'Re-Order', $userdata->user_level, 'order-post-types-'.$post_type_name, array(&$this, 'SortPage') );
                         else
-                            add_submenu_page('edit.php?post_type='.$post_type_name, 'Re-Order', 'Re-Order', $userdata->user_level, 'order-post-type-'.$post_type_name, array(&$this, 'pageManage') );
+                            add_submenu_page('edit.php?post_type='.$post_type_name, 'Re-Order', 'Re-Order', $userdata->user_level, 'order-post-types-'.$post_type_name, array(&$this, 'SortPage') );
 		            }
 	        }
 	    
 
-	    function pageManage() {
-		    ?>
-		    <div class="wrap">
-			    <div class="icon32" id="icon-edit"><br></div>
-                <h2><?php echo $this->current_post_type->labels->singular_name . ' -  Re-order '?></h2>
+	    function SortPage() 
+            {
+		        ?>
+		        <div class="wrap">
+			        <div class="icon32" id="icon-edit"><br></div>
+                    <h2><?php echo $this->current_post_type->labels->singular_name . ' -  Re-order '?></h2>
 
-			    <div id="ajax-response"></div>
-			    
-			    <noscript>
-				    <div class="error message">
-					    <p>This plugin can't work without javascript, because it's use drag and drop and AJAX.</p>
-				    </div>
-			    </noscript>
-			    
-			    <div id="order-post-type">
-				    <ul id="sortable">
-					    <?php $this->listPages('hide_empty=0&title_li=&post_type='.$this->current_post_type->name); ?>
-				    </ul>
-				    
-				    <div class="clear"></div>
-			    </div>
-			    
-			    <p class="submit">
-				    <a href="#" id="save-order" class="button-primary">Update</a>
-			    </p>
-			    
-			    <script type="text/javascript">
-				    jQuery(document).ready(function() {
-					    jQuery("#sortable").sortable({
-						    'tolerance':'intersect',
-						    'cursor':'pointer',
-                            'grid': [50, 10],
-						    'items':'li',
-						    'placeholder':'placeholder',
-						    'nested': 'ul'
-					    });
-					    
-					    jQuery("#sortable").disableSelection();
-					    jQuery("#save-order").bind( "click", function() {
-						    jQuery.post( ajaxurl, { action:'update-custom-type-order', order:jQuery("#sortable").sortable("serialize") }, function() {
-							    jQuery("#ajax-response").html('<div class="message updated fade"><p>Items Order Updates</p></div>');
-							    jQuery("#ajax-response div").delay(3000).hide("slow");
-						    });
-					    });
-				    });
-			    </script>
-                
-                
-                <h3>Did you found this plug-in useful? Please support our work with a donation.</h3>
-                <div id="donate_form">
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="CU22TFDKJMLAE">
-                    <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
-                </div>
-                <br />
-		    </div>
-		    <?php
-	    }
+			        <div id="ajax-response"></div>
+			        
+			        <noscript>
+				        <div class="error message">
+					        <p>This plugin can't work without javascript, because it's use drag and drop and AJAX.</p>
+				        </div>
+			        </noscript>
+			        
+			        <div id="order-post-type">
+				        <ul id="sortable">
+					        <?php $this->listPages('hide_empty=0&title_li=&post_type='.$this->current_post_type->name); ?>
+				        </ul>
+				        
+				        <div class="clear"></div>
+			        </div>
+			        
+			        <p class="submit">
+				        <a href="#" id="save-order" class="button-primary">Update</a>
+			        </p>
+			        
+			        <script type="text/javascript">
+				        jQuery(document).ready(function() {
+					        jQuery("#sortable").sortable({
+						        'tolerance':'intersect',
+						        'cursor':'pointer',
+                                'grid': [50, 10],
+						        'items':'li',
+						        'placeholder':'placeholder',
+						        'nested': 'ul'
+					        });
+					        
+					        jQuery("#sortable").disableSelection();
+					        jQuery("#save-order").bind( "click", function() {
+						        jQuery.post( ajaxurl, { action:'update-custom-type-order', order:jQuery("#sortable").sortable("serialize") }, function() {
+							        jQuery("#ajax-response").html('<div class="message updated fade"><p>Items Order Updates</p></div>');
+							        jQuery("#ajax-response div").delay(3000).hide("slow");
+						        });
+					        });
+				        });
+			        </script>
+                    
+                    
+                    <h3>Did you found this plug-in useful? Please support our work with a donation.</h3>
+                    <div id="donate_form">
+                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <input type="hidden" name="hosted_button_id" value="CU22TFDKJMLAE">
+                        <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                        </form>
+                    </div>
+                    <br />
+		        </div>
+		        <?php
+	        }
 
 	    function listPages($args = '') 
             {
@@ -297,7 +331,9 @@ class CPT
                 $args = array(
                             'sort_column'   =>  'menu_order',
                             'post_type'     =>  $post_type,
-                            'posts_per_page' => -1
+                            'posts_per_page' => -1,
+                            'orderby'        => 'menu_order',
+                            'order'         => 'ASC'
                 );
                 
                 $the_query = new WP_Query($args);
@@ -324,7 +360,7 @@ class CPT
 	    function walkTree($pages, $depth, $r) 
             {
 		        if ( empty($r['walker']) )
-			        $walker = new Custom_Type_Order_Walker;
+			        $walker = new Post_Types_Order_Walker;
 		        else
 			        $walker = $r['walker'];
 
